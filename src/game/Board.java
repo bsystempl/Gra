f193ae2e297a5b17a11f9d531b344f7fce59cfce
bsystempl <bsystempl@gmail.com>
2015-05-20 20:58:47 +0200

    SZYBCIEJ

--- src/game/Board.java
+++ src/game/Board.java
@@ -12,6 +12,7 @@ public class Board extends JPanel implements ActionListener {
     public Image img;
     public Image img1;
     Timer time;
+    Integer punkty;
 
 
 
@@ -29,34 +30,42 @@ public class Board extends JPanel implements ActionListener {
         img1 = loser.getImage();
         time = new Timer(5, this);
         time.start();
+        punkty = p.z;
     }
 
     public void actionPerformed(ActionEvent e) {
-        CheckAlive();
-        p.move();
-        p.moveY();
-        u.move();
-        o.move();
+            CheckAlive();
+            p.move();
+            p.moveY();
+            u.move();
+            o.move();
 
-        if(p.y > 150)
-        {
-            p.dy = 0;
-        }
+            if(punkty > 300 )
+            {
+                punkty = 0;
+                u.dx++;
+                o.dx++;
+            }
 
-        if(p.y <= 0)
-        {
-            p.dy = 2;
-        }
+            if(p.y > 150)
+            {
+                p.dy = 0;
+            }
+
+            if(p.y <= 0)
+            {
+                p.dy = 2;
+            }
 
 
-        if(o.x < 0 && u.x < 0)
-        {
-            Random generator = new Random();
-            int a = p.getX()+400;
-            o.x = a;
-            u.x = a;
-        }
-        repaint();
+            if(o.x < 0 && u.x < 0)
+            {
+                Random generator = new Random();
+                int a = p.getX()+400;
+                o.x = a;
+                u.x = a;
+            }
+            repaint();
     }
 
     public void paint(Graphics g) {
@@ -77,6 +86,8 @@ public class Board extends JPanel implements ActionListener {
             g2d.drawImage(img1, 0, 0, null);
             g2d.drawString("twoje punkty: " + p.z.toString(), 600, 20);
         }
+
+
     }
 
     private class AL extends KeyAdapter {
@@ -108,6 +119,7 @@ public class Board extends JPanel implements ActionListener {
         if(p.getBounds().getX() > o.getBounds().getX() && p.getBounds().getX() < o.getBounds().getX() + 60 && p.alive == true ){
                 System.out.println("przeskoczono!");
                 p.z = p.z + 1;
+                punkty ++;
             }
 
 
